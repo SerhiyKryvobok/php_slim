f5982efb3429ee421aed1a47d82d1631d950d172
serhiy <serhiy.kryvobok@gmail.com>
2025-04-01 01:12:49 +0300

    Basic implementation of router, deps, config and so on

---
+++ app/Controllers/FirstController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class FirstController extends BaseController
+{
+    public function home($request, $response, $args)
+    {
+        // $data = $request->getQueryParams(); // GET data like ?param=something
+        // $data = $request->getParsedData(); // POST data
+        $response->getBody()->write($this->container->my_service);
+        $response->getBody()->write($this->container['settings']['db']['user']);
+        $data = [
+            ['name' => 'Adam', 'id' => 1],
+            ['name' => 'John', 'id' => 2],
+        ];
+        $response = $this->container->view->render($response, 'view.phtml', ['name' => $args['name'], 'data' => $data]);
+        return $response;
+    }
+}
